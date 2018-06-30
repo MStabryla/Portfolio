dc0a1d56de866c3809f235926432bd56f0c929bb
mstabryla <mstabryla@op.pl>
2018-06-30 16:58:56 +0200

    Creating front page

--- client/app/main.component.ts
+++ client/app/main.component.ts
@@ -10,4 +10,11 @@ import { Component } from '@angular/core';
 export class MainComponent {
     public MainIns: string = "Mateusz Stabryła";
     public Desc: string = "Full Stack .NET Developer";
+    scroll(): void {
+        window.scrollBy({
+            left: 0,
+            top: window.innerHeight,
+            behavior: 'smooth'
+        })
+    }
 }
--- client/app/menu.component.ts
+++ client/app/menu.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
     ]
 })
 export class MenuComponent {
-    Logo: string = "Mateusz Stabryła"
+    Logo: string = "Mateusz  Stabryła"
 }
