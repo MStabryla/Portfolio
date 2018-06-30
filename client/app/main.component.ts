2d0be341a58685f4e1d3cd96b762e50c44d76b9b
mstabryla <mstabryla@op.pl>
2018-06-30 23:32:49 +0200

    Scroll and JS font color

--- client/app/main.component.ts
+++ client/app/main.component.ts
@@ -19,20 +19,32 @@ export class MainComponent {
     public MainIns: string = "Mateusz Stabryła";
     public Desc: string = "Full Stack .NET Developer";
     scroll(): void {
-        /*window.scrollBy({
-            left: 0,
-            top: window.innerHeight,
-            behavior: 'smooth'
-        })*/
         window.addEventListener("scroll",e => {
-            //event.returnValue = false;
             e.returnValue = false
             e.preventDefault ? e.preventDefault() : (e.returnValue = false);
         })
-        let distanse = window.innerHeight,speed = 12;
+        let destiny = window.innerHeight,speed = 12,distanse = Math.abs(destiny - window.scrollY),distanseCounter = 0;
+        console.log("distanse",distanse);
         let scrollInterval = setInterval(() => {
             window.scrollBy(0,speed);
-            if(window.scrollY >= distanse)
+            distanseCounter += speed;
+            if(distanseCounter >=  distanse * 0.4 && distanseCounter <= distanse * 0.6)
+            {
+                speed = 18;
+            }
+            else if(distanseCounter <  distanse * 0.3 || distanseCounter > distanse * 0.7)
+            {
+                speed = 12;
+            }
+            if(distanseCounter <  distanse * 0.2 || distanseCounter > distanse * 0.8)
+            {
+                speed = 10;
+            }
+            if(distanseCounter <  distanse * 0.1 || distanseCounter > distanse * 0.9)
+            {
+                speed = 4;
+            }
+            if(window.scrollY >= destiny)
             {
                 clearInterval(scrollInterval);
                 window.onscroll = null;
